4ca2ca7fd464b56e4f86b3f1eb4e5b8cb0ec97dc
ikimhr <rizkimuharamgugus10@gmail.com>
2025-11-11 01:01:19 +0700

    Login Page

---
+++ lib/Pages/auth/login_page.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: 
+      Stack(
+        children: [
+          Container(
+            height: 270,
+            color: Colors.grey[700],
+          ),
+          Container(
+          margin: EdgeInsets.only(top: 200, left: 100, right: 100),
+          child: Column(
+            children: [
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+              ), 
+              SizedBox(height: 3),
+        
+               Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+              ),
+              SizedBox(height: 20),
+              
+              Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Container(
+                  margin: EdgeInsets.only(left: 7, top: 7, bottom: 7, right: 200),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[700],
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+              ), 
+              SizedBox(height: 20),
+              
+              Container(
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 7, horizontal: 50),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[700],
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+        
+              Container(
+                padding: EdgeInsets.all(7),
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Stack(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 16,
+                          margin: EdgeInsets.only(right: 7),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[700],
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        ),
+                        
+                        Container(
+                          width: 260,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[700],
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        ),
+        
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 4),
+              
+              Container(
+                padding: EdgeInsets.all(7),
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Stack(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 16,
+                          margin: EdgeInsets.only(right: 7),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[700],
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        ),
+                        
+                        Container(
+                          width: 260,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[700],
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        ),
+        
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 15,),
+        
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 10,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(5)
+                      ),
+                    ), SizedBox(height: 2,),
+                    Container(
+                      height: 10,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(5)
+                      ),
+                    ), SizedBox(height: 2,),
+                    Container(
+                      height: 10,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(5)
+                      ),
+                    ), SizedBox(height: 2,),
+                    Container(
+                      height: 10,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(5)
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              )
+        
+            ],
+          ),
+        ),
+        ],
+      )
+    );
+  }
+}
